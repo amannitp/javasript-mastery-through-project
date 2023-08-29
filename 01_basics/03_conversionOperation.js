ffed8f12e18e4837eea5bc86d60d2b92e1eac9ac
amannitp <amannitp272@gmail.com>
2023-08-29 11:30:15 +0530

    operation in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,23 +17,52 @@ let  valueInNumber=Number(50)
 
 let boolNum=Boolean(1)
 let boolNum2=Boolean(0)
-console.log(boolNum)  // true 
-console.log(boolNum2) // false 
+// console.log(boolNum)  // true 
+// console.log(boolNum2) // false 
 
 let boolStr=Boolean(""); // false
 let boolStr2=Boolean(" Aman RaJ")  //true 
 
 
-console.log(boolStr)
-console.log(boolStr2)
+// console.log(boolStr)
+// console.log(boolStr2)
 
-console.log(Boolean(null))  // false 
-console.log(Boolean(undefined)) // false
+// console.log(Boolean(null))  // false 
+// console.log(Boolean(undefined)) // false
 
 
-console.log(String(null))  // null
-console.log(String(undefined)) // undefined
+// console.log(String(null))  // null
+// console.log(String(undefined)) // undefined
 
 //console.log( typeof valueInNumber)
 //console.log(typeof score)
 //onsole.log(typeof(score));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++ Operation+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2%2)
+// console.log(2/2)
+
+let str1=" Aman"
+let str2=" Raj"
+
+console.log(str1+str2)
+
+console.log("1"+2)//12
+console.log(1+"2"); //12
+console.log("1"+2+2) //122
+console.log(1+2+"3") // 33
+
+console.log((1+2)**5)  // use parethesis when you you work on any project
+
+console.log(+true) //1
+console.log(+"") // 0
+
+// let x=5;
+let y=x++;
+let z=++y;
+
