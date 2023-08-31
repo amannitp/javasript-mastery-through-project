ab6169da848b191b3178b187b9261d610a19f65c
amannitp <amannitp272@gmail.com>
2023-08-31 16:13:32 +0530

    functionscope part1

---
+++ 03_basics/02_function_scope.js
@@ -0,0 +1,28 @@
+
+
+// let a=20;
+// const b=30;
+// var c=40
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// if(true){
+// let a=20;
+// const b=30;
+// var c=40
+// }
+// console.log(a)  error
+// console.log(b)  error
+
+//console.log(c) // 40
+
+function scope(){
+  let a=20;
+  const b=30;
+  var c=40
+}
+scope()
+// console.log(a) eroor 
+// console.log(b)  error 
+// console.log(c)  error  because var is functional scope 
