c7255510d3ec0aa33ead541b69b7469d1e8bd975
amannitp <amannitp272@gmail.com>
2023-09-01 21:55:49 +0530

    coalincing operator and its uses

--- 04_basics/02_truthy_falsy.js
+++ 04_basics/02_truthy_falsy.js
@@ -58,3 +58,24 @@ if(Object.values(myObj).length===0){
     console.log(" this way you can check empty object")
 }
 
+
+
+// nullish coacheling operator use for detect error
+
+let val1;
+
+val1=10??5 // 10
+val1=null ?? 5 // 5
+console.log(val1)
+
+let val2=undefined
+
+val2=20?? 30 // 20
+val2=undefined?? 30 // 20
+val2=null??40??50   //40
+console.log(val2)  
+
+
+if(true) console.log(" you can also remove curly brace only for 1 statement"),console.log(" this is also valid");
+
+for(let i=0;i>5;i++) console.log(" valid statement") // not print anything 
