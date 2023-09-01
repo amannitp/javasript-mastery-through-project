867a7f373835ecf970a9ead3ef8c978f2a8a28ee
amannitp <amannitp272@gmail.com>
2023-09-01 21:15:02 +0530

    swith case use in redux

--- 04_basics/01_controlflow.js
+++ 04_basics/01_controlflow.js
@@ -10,3 +10,18 @@
 //     console.log(" please enter your name")
     
 // }
+  const month ="march"
+switch (month) {
+    case "january":
+        console.log("January")
+        
+        break;
+    case "feb":
+        console.log("feb")
+        
+        break;
+
+    default:
+        console.log("default value")
+        break;
+}
