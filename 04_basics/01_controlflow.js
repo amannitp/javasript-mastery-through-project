c980f2f5a61a1fdd65a87010610467313f7f7380
amannitp <amannitp272@gmail.com>
2023-09-01 21:12:54 +0530

    if else

---
+++ 04_basics/01_controlflow.js
@@ -0,0 +1,12 @@
+// if (true) {
+//     console.log('condition is true')
+    
+// }
+
+// if (true) {
+//     console.log(" hello world")
+    
+// } else {
+//     console.log(" please enter your name")
+    
+// }
