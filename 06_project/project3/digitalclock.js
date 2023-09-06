2635955f389f3acb25e8d83870697dff588d243a
amannitp <amannitp272@gmail.com>
2023-09-06 15:27:19 +0530

    Digital clock logic

---
+++ 06_project/project3/digitalclock.js
@@ -0,0 +1,11 @@
+
+
+const clock=document.querySelector('.clock')
+
+
+
+setInterval(()=>{
+    let time=new Date()
+    //console.log(time.toLocaleTimeString())
+    clock.innerHTML=time.toLocaleTimeString()
+},1000)
