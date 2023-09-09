ba6431f6332d084dd07f290b66053f161bb89d45
amannitp <amannitp272@gmail.com>
2023-09-09 22:18:35 +0530

    promise

--- 09_promise/01_promises.js
+++ 09_promise/01_promises.js
@@ -1,158 +1,159 @@
 
 
-// 1st way
+//1st way
 
-// const promiseOne=new Promise(function(resolve,reject){
+const promiseOne=new Promise(function(resolve,reject){
 
-//     // Asymc work 
-//     // db connected , network call
+    // Asymc work 
+    // db connected , network call
 
-//     setTimeout(function(){
-//       //  resolve()
-//         console.log(" Async task is completed ")
-//         resolve()
+    setTimeout(function(){
+      //  resolve()
+        console.log(" Async task is completed ")
+        resolve()
         
-//     },1000)
-// })
+    },1000)
+})
 
-// promiseOne.then(function(){
+promiseOne.then(function(){
    
-//     console.log(" Promises is fullfilled ")
+    console.log(" Promises is fullfilled ")
 
-// })
-
-
-// // 2nd way
-
-// new Promise(function(resolve,reject){
+})
 
 
-//     setTimeout(function(){
-//         console.log(" Async 2 is completed")
-//         resolve()
-//     },1000)
+// 2nd way
 
-// }).then(function(){
-
-//     console.log("Async 2 task is fullfilled")
-// })
-
-// // 3rd way
-
-// const promisesThree=new Promise(function(resolve,reject){
-
-//     setTimeout(function(){
-//         console.log(" Astnc 3rd Task is completed")
-//         resolve({name:"Aman",RollNo:1902036})
-//     },1000)
-// })
-
-// promisesThree.then(function(user){
-
-//     console.log(user)
-// })
-
-// // 4th way
-
-// const promiseFour=new Promise(function(resolve,reject){
-
-//     let error=false;
-
-//     if(!error){
-//         resolve({username:"Aman",password:"123@123"})
-//     }else{
-//         reject(" Something went Wrong")
-//     }
-// })
-
-// promiseFour
-// .then(function(user){
-
-//     console.log(user)
-//     return user.username;  // chaining 
-// })
-// .then(function(userName){
-//     console.log(userName) // here return the promise of first one 
-// })
-// .catch(function(error){
-//     console.log(error)
-// })
-// .finally(()=>{
-//     console.log("Either promise is resolved or promise are rejected")
-// })
-
-// // 5 way
-
-// const promiseFive=new Promise((resolve,reject)=>{
-
-//     let error=false;
-
-//     if(!error){
-//         resolve({username:"javascript",password:"123@123"})
-//     }else{
-//         reject(" java went Wrong")
-//     }
+new Promise(function(resolve,reject){
 
 
-// })
+    setTimeout(function(){
+        console.log(" Async 2 is completed")
+        resolve()
+    },1000)
 
-// async function consumePromiseFive(){  // it not capture error
+}).then(function(){
 
-//     const response= await promiseFive
-//     console.log(response)
-// }
+    console.log("Async 2 task is fullfilled")
+})
 
-// consumePromiseFive()
+// 3rd way
+
+const promisesThree=new Promise(function(resolve,reject){
+
+    setTimeout(function(){
+        console.log(" Astnc 3rd Task is completed")
+        resolve({name:"Aman",RollNo:1902036})
+    },1000)
+})
+
+promisesThree.then(function(user){
+
+    console.log(user)
+})
+
+// 4th way
+
+const promiseFour=new Promise(function(resolve,reject){
+
+    let error=false;
+
+    if(!error){
+        resolve({username:"Aman",password:"123@123"})
+    }else{
+        reject(" Something went Wrong")
+    }
+})
+
+promiseFour
+.then(function(user){
+
+    console.log(user)
+    return user.username;  // chaining 
+})
+.then(function(userName){
+    console.log(userName) // here return the promise of first one 
+})
+.catch(function(error){
+    console.log(error)
+})
+.finally(()=>{
+    console.log("Either promise is resolved or promise are rejected")
+})
+
+// 5 way
+
+const promiseFive=new Promise((resolve,reject)=>{
+
+    let error=false;
+
+    if(!error){
+        resolve({username:"javascript",password:"123@123"})
+    }else{
+        reject(" java went Wrong")
+    }
 
 
-// // 6th way
+})
 
-// const promiseSix=new Promise((resolve,reject)=>{
+async function consumePromiseFive(){  // it not capture error
 
-//     let error=true;
+    const response= await promiseFive
+    console.log(response)
+}
 
-//     if(!error){
-//         resolve({username:"javascript again",password:"123@123"})
-//     }else{
-//         reject(" javaagain went Wrong")
-//     }
+consumePromiseFive()
 
 
-// })
+// 6th way
 
-// async function consumePromiseSix(){  
-//    try{
-//     const response= await promiseSix
-//     console.log(response)
+const promiseSix=new Promise((resolve,reject)=>{
 
-//    }catch(error){
-//         console.log(error)  // here error is catch
-//    }
+    let error=true;
+
+    if(!error){
+        resolve({username:"javascript again",password:"123@123"})
+    }else{
+        reject(" javaagain went Wrong")
+    }
+
+
+})
+
+async function consumePromiseSix(){  
+   try{
+    const response= await promiseSix
+    console.log(response)
+
+   }catch(error){
+        console.log(error)  // here error is catch
+   }
    
-// }
+}
 
-// consumePromiseSix()
+consumePromiseSix()
 
 
 
-// best way to use 
-// async function getData(){
 
-//     try {
-//         const response=await fetch('https://randomuser.me/api')
-//         const data= await response.json()
-//         console.log(data)
+//best way to use 
+async function getData(){
+
+    try {
+        const response=await fetch('https://randomuser.me/api')
+        const data= await response.json()
+        console.log(data)
         
-//     } catch (error) {
+    } catch (error) {
 
-//         console.log("E: ",error)
+        console.log("E: ",error)
         
-//     }
-// }
+    }
+}
 
-// getData()
+getData()
 
-// 7th way
+//7th way
 
 fetch('https://randomuser.me/api')
 .then((response)=>{
