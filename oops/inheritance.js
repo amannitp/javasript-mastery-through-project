295de47964409a8d035d8e995c3ec2852ea892e6
amannitp <amannitp272@gmail.com>
2023-09-11 10:55:10 +0530

    inheritance

---
+++ oops/inheritance.js
@@ -0,0 +1,31 @@
+class user{
+    constructor(username){
+        this.username=username
+    }
+    showMe(){
+        console.log(`UserName is ${this.username}`)
+    }
+}
+
+class Teacher extends user{
+    constructor(username,email,password){
+        super(username)
+        this.email=email;
+        this.password=password
+    }
+
+    addCourses(){
+        console.log(`course is added by ${this.username}`)
+    }
+}
+
+const teacher1=new Teacher("Aman","aman@123","12345455")
+const user1=new user(" Raj")
+
+teacher1.addCourses()
+teacher1.showMe()
+user1.showMe()
+console.log(teacher1===user1)
+console.log(teacher1 instanceof user)
+console.log(user1 instanceof Teacher)
+
